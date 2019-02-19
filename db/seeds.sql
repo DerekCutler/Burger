a11b909e2f14b77db0174eb51e56c0ae20af74ba
Derek P. Cutler <43916483+DerekCutler@users.noreply.github.com>
2019-02-19 16:34:36 -0700

    DB Setup

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Crown Burger'),
+    ('Junior Crown Burger'),
+    ('Bacon Cheese Burger'),
+    ('Jr. Burger'),
+    ('Hamburger	'),
+    ('Cheeseburger'),
+    ('DBL Hamburger');
