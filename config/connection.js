a24d2a04413a497dadccf7871b75b293672a3af4
Derek P. Cutler <43916483+DerekCutler@users.noreply.github.com>
2019-02-26 21:07:38 -0700

    adding JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -8,13 +8,26 @@ var mysql = require("mysql");
 // ==========================================
 // MAKE CONNECTION
 // ==========================================
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Password",
+//     database: "burgers_db"
+//   });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    // port: 3306,
     user: "root",
     password: "Password",
     database: "burgers_db"
   });
+};
+
 
 connection.connect(function(err) {
   if (err) {
